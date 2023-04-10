a1e2b46a71f309797e73bda2ec1c4aad06c3ac13
PJ Palomaki <2419138+pjburnhill@users.noreply.github.com>
2023-04-10 14:59:09 +0100

    Init

---
+++ Scenes/Player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export var ACCELERATION = 500
+@export var MAX_SPEED = 80
+@export var FRICTION = 700
+
+var VEC_MAX_SPEED = Vector2(MAX_SPEED, MAX_SPEED)
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
+		velocity = velocity.move_toward(input_vector * VEC_MAX_SPEED, ACCELERATION * delta)
+		#vel += input_vector * ACCELERATION * delta
+		#vel = vel.clamp(MIN_SPEED, MAX_SPEED)
+		print(velocity)
+	else:
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	move_and_slide()
