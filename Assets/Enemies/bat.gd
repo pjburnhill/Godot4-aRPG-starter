4456095409abfaa904d8a229d71349eff9f9c33d
PJ Palomaki <2419138+pjburnhill@users.noreply.github.com>
2023-04-16 22:43:47 +0100

    Bat AI P1

--- Assets/Enemies/bat.gd
+++ Assets/Enemies/bat.gd
@@ -1,20 +1,59 @@
 extends CharacterBody2D
 
 @onready var stats = $Stats
+@onready var sprite = $AnimatedSprite
+@onready var playerDetectionZone = $PlayerDetectionZone
+
 const EnemyDeathEffect = preload("res://Assets/Effects/enemy_death_effect.tscn")
+
+@export var ACCELERATION = 250
+@export var MAX_SPEED = 20
+@export var FRICTION = 50
+@export var KNOCKBACK_VELOCITY = 80
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var state = CHASE
 
 func _ready():
 	print('Health_Max: ', stats.max_health)
 	print('Health: ', stats.health)
-
+	
 func _physics_process(delta):
-	velocity = velocity.move_toward(Vector2.ZERO, delta * 200)
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	move_and_slide()
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+			
+		WANDER:
+			pass
+			
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+				
+	move_and_slide()
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_hurtbox_area_entered(area):
 	stats.health -= area.damage
 	print('Health: ', stats.health)
-	velocity = area.damage_vector * 120
+	velocity = area.damage_vector * KNOCKBACK_VELOCITY
 
 func _on_stats_no_health():
 	var enemyDeathEffect = EnemyDeathEffect.instantiate()
